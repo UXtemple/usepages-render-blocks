dab39f959a11bda61612bc947692ac02fdae46aa
Darío Javier Cravero <dario@uxtemple.com>
2016-02-07 13:00:42 +0000

    fix: remove leftover debugger statement

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 export default function render(blocks, sources, keyPrefix) {
   return blocks.map((block, i) => {
-    const Block = sources[block.type] || sources.Unknown;
-    return <Block {...block.props} key={`${keyPrefix}-${i}`} />;
+    try {
+      const Block = sources[block.type] || sources.Unknown;
+      return <Block {...block.props} key={`${keyPrefix}-${i}`} />;
+    } catch(err) {
+      const givenProps = Object.keys(block.props)
+        .map(key => <span key={key}>`${key}: ${block.props[key]}`</span>).join('\n');
+
+      return (
+        <div style={style.error}>
+          We couldn't render your block {block.type}.
+          You gave it these props:
+          ${givenProps}
+
+          This is what failed:
+          {err.message}
+          {err.stack}
+        </div>
+      );
+    }
   });
+}
+
+const style = {
+  error: {
+    backgroundColor: '#ff5959',
+    color: 'white',
+    fontSize: 12
+  }
 }
